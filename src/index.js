a4b2d0bbb7d10d3c858e2c5d7f58077ea33200c1
harshith2303 <harshithe00@gmail.com>
2025-05-22 20:19:48 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+
+
+import { useEffect, useState } from "react";
+
+export function App() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    fetch(
+      "https://countries-search-data-prod-812920491762.asia-south1.run.app/countries"
+    )
+      .then((rawData) => rawData.json().then((finalData) => setData(finalData)))
+      .catch((error) => console.error("got wrong" + error));
+  }, []);
+  let filteredData = data.filter((ele) =>
+    ele.common.toLowerCase().includes(search.toLowerCase())
+  );
+  return (
+    <div style={{textAlign:"center"}}>
+      <input
+        type="text"
+        placeholder="Search countries..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div style={{display:"flex",
+      flexWrap:"wrap"
+      
+      }}>
+      {filteredData.map((ele) => (
+        <div
+          style={{
+            border: "2px solid black",
+            width: "150px",
+            margin: "10px",
+            borderRadius: "10px",
+            textAlign:"center"
+          }}
+        >
+          <img
+            style={{ width: "100px", height: "100px", margin: "10px"}}
+            src={ele.png}
+            alt={ele.common}
+          />
+          <h4>{ele.common}</h4>
+        </div>
+      ))}
+      </div>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +61,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
